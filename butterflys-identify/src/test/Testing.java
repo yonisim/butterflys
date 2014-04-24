53c50acf24522ce058676aa650cad40902d06e10
yonisim <yonisim@gmail.com>
2014-04-24 08:43:57 +0300

    added validation on butterfly insert

--- butterflys-identify/src/test/Testing.java
+++ butterflys-identify/src/test/Testing.java
@@ -13,18 +13,21 @@ public class Testing {
 
 	
 	public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
-		MysqlClient mySqlClient = new MysqlClient();
-		String query1 = "SELECT * FROM butterflys.butterflys_general";
-
-
-		mySqlClient.executeQuery(query1);
-		mySqlClient.printFirstColFromResultSet();
-		
-		Butterfly butterfly = new Butterfly("pink4", "pink4" , "this butterfly is very very pink");
 		ButterflysClient client = new ButterflysClient();
-	//	client.insertButterfly(butterfly);
 		
 		List<Butterfly> list = client.selectButterfly("");
+		System.out.println(list.size());
+		for(Butterfly b : list){
+			System.out.println(b.toString());
+		}
+		
+		Butterfly butterfly = new Butterfly("pink7", "pink7" , "this butterfly is very very pink");
+		while(client.insertButterfly(butterfly)){
+			butterfly.name += 1;
+			butterfly.linkToPicture += 1;
+		}
+		
+		list = client.selectButterfly("");
 		System.out.println(list.size());
 		for(Butterfly b : list){
 			System.out.println(b.toString());
